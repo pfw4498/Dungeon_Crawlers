ee33ab1292bca4cb052d211f87e7ba1b7bcd4393
Tory-PC\Tory <tjl1080@rit.edu>
2016-11-12 15:42:01 -0500

    uhhhhhhhh

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,60 @@ public class Player : MonoBehaviour
 	public GameObject aimDevice;
 	public GameObject bulletSpawn;
 	public GameObject bullet;
-
+	public GameObject shield;
+	
 	public int health;
 	public int mana;
+	public const int MAXMANA = 15;
+	public const int MANAINCR = 1;
+	public const int MAXHEALTH = 15;
+	public const int HEALTHINCR = 1;
+	public float speed = 2f;
 
+	private Transform tm;
+
+	public KeyCode up, down, left, right, space;
 	// Use this for initialization
-	void Start () 
+	void Awake () 
 	{
-		
+		shield.gameObject.SetActive(false);
+		tm = GetComponent<Transform>();
 	}
-	
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if(coll.gameObject.tag == "Health" && health < MAXHEALTH) {
+			health += HEALTHINCR;
+			Destroy(coll.gameObject);
+		}
+		if(coll.gameObject.tag == "Mana" && mana < MAXMANA) {
+			mana += MANAINCR;
+			Destroy(coll.gameObject);
+		}
+	}
 	// Update is called once per frame
 	void Update () 
 	{
+		// set shield to active
+		if(Input.GetKey(space)) {
+			Debug.Log ("aaa");
+			shield.gameObject.SetActive(true);
+		} else {
+			Debug.Log("ggg");
+			shield.gameObject.SetActive(false);
+		}
+
+		// general movement
+		if(Input.GetKey(up)) {
+			tm.Translate(Vector2.up * speed);
+		} else if(Input.GetKey(down)) {
+			tm.Translate(Vector2.down * speed);
+		}
+		if(Input.GetKey(left)) {
+			tm.Translate(Vector2.left * speed);
+
+		} else if(Input.GetKey(right)) {
+			tm.Translate(Vector2.right * speed);
+		}
 		Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		
 		float angle = Mathf.Atan2(mouseWorldPos.y, mouseWorldPos.x) * Mathf.Rad2Deg;
---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+	private Transform tm;
+	public KeyCode up, down, left, right;
+	bool rightleft = true;
+
+
+	// Use this for initialization
+	void Start () {
+		tm = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(up)) {
+			if(rightleft==true)
+			{
+				tm.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+				tm.position = new Vector2(tm.parent.position.x + 0, tm.parent.position.y + .7f);
+				rightleft=false;
+			}
+			else{
+				tm.position = new Vector2(tm.parent.position.x + 0, tm.parent.position.y + .7f);
+				rightleft=false;
+			}
+		} else if(Input.GetKey(down)) {
+			if(rightleft==true)
+			{
+				tm.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+				tm.position = new Vector2(tm.parent.position.x + 0, tm.parent.position.y - .7f);
+				rightleft=false;
+			}
+			else{
+				tm.position = new Vector2(tm.parent.position.x + 0, tm.parent.position.y - .7f);
+				rightleft=false;
+			}
+
+
+		} else if(Input.GetKey(left)) {
+			if(rightleft==false)
+			{
+				tm.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+				tm.position = new Vector2(tm.parent.position.x -.7f, tm.parent.position.y );
+				rightleft=true;
+			}
+			else{
+				tm.position = new Vector2(tm.parent.position.x -.7f, tm.parent.position.y);
+				rightleft=true;
+			}
+			
+		} else if(Input.GetKey(right)) {
+			if(rightleft==false)
+			{
+				tm.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+				tm.position = new Vector2(tm.parent.position.x + .7f, tm.parent.position.y);
+				rightleft=true;
+			}
+			else{
+				tm.position = new Vector2(tm.parent.position.x + .7f, tm.parent.position.y);
+				rightleft=true;
+			}
+		}
+	}
+}
